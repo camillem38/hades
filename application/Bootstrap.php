0541224c54536c3d30e7851c7ca84d902f2676a4
Maxime Jonckiere <maxime.jonckiere@gmail.com>
2013-08-07 14:07:41 +0200

    Retour à zend 1, mise en place du layout avec Initializr

---
+++ application/Bootstrap.php
@@ -0,0 +1,50 @@
+<?php
+
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
+	
+	protected function _initAutoLoader() {
+		/*
+		 * $autoloader = Zend_Loader_Autoloader::getInstance();
+		 * $autoloader->setFallbackAutoloader(true); return $autoloader;
+		 */
+	}
+	
+	protected function _initView() {
+		$view = new Zend_View();
+		$view->env = APPLICATION_ENV;
+		$view->doctype('HTML5');
+		$view->setEncoding('utf-8');
+	
+		$view->headMeta()->setCharset('utf-8')
+		                 ->appendHttpEquiv('X-UA-Compatible', 'IE=edge,chrome=1')
+		                 ->appendName ( 'viewport', 'width=device-width' );
+		
+		$view->headTitle()->setSeparator ( ' - ' );
+		$view->headTitle( 'Hades' );
+		
+		$view->headLink()->appendStylesheet( '/css/bootstrap.min.css')
+		                 ->appendStylesheet('/css/bootstrap-responsive.min.css')
+		                 ->appendStylesheet ('/css/main.css');
+		
+		$view->headScript()->appendFile('/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js');
+		
+		$view->inlineScript()->appendFile('//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js')
+                             ->appendScript('window.jQuery || document.write(\'<script src="js/vendor/jquery-1.10.1.min.js"><\/script>\')')
+		                     ->appendFile ( '/js/vendor/bootstrap.min.js' )
+		                     ->appendFile ( '/js/vendor/main.js' );
+		
+		
+		$view->addHelperPath(APPLICATION_PATH . '/modules/home/views/helpers', 'Zend_view_helper');
+		// Add it to the ViewRenderer
+		$viewRenderer = Zend_Controller_Action_HelperBroker::getStaticHelper('ViewRenderer');
+		$viewRenderer->setView($view);
+		
+		return $view;
+	}
+	
+	protected function _initViewHelpers() {
+		Zend_Paginator::setDefaultScrollingStyle('Sliding');
+		Zend_View_Helper_PaginationControl::setDefaultViewPartial('pagination.phtml');
+	}
+}
+
--- public/index.php
+++ public/index.php
@@ -1,12 +1,51 @@
 <?php
-/**
- * This makes our life easier when dealing with paths. Everything is relative
- * to the application root now.
- */
-chdir(dirname(__DIR__));
 
-// Setup autoloading
-require 'init_autoloader.php';
+// Define path to application directory
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
 
-// Run the application!
-Zend\Mvc\Application::init(require 'config/application.config.php')->run();
+// Define application environment
+defined('APPLICATION_ENV')
+    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+
+defined('FONTS_PATH')
+//    || define('FONTS_PATH', 'C:/Windows/Fonts/');
+    || define('FONTS_PATH', APPLICATION_PATH . '/../public/fonts/');
+
+defined('IMAGE_URL')
+    || define('IMAGE_URL', "http://127.0.0.1/hades/images/");
+
+// Ensure library/ is on include_path
+set_include_path(implode(PATH_SEPARATOR, array(
+    
+    realpath(APPLICATION_PATH . '/../library'),
+    get_include_path(),
+)));
+
+/** Zend_Application */
+require_once 'Zend/Application.php'; 
+
+require_once 'Zend/Loader/Autoloader.php';
+
+$loader = Zend_Loader_Autoloader::getInstance();
+
+$loader->registerNamespace('App_');;
+
+Zend_Session::start();
+/*
+$config = new Zend_Config_Ini('/../application/configs/db_config.ini', 'offline');
+$registry = Zend_Registry::getInstance();
+$registry->set('db_config',$config);
+$db_config = Zend_Registry::get('db_config');
+$db = Zend_Db::factory($db_config->db);
+Zend_Db_Table::setDefaultAdapter($db);
+$db->query("SET NAMES 'utf8'");*/
+
+
+// Create application, bootstrap, and run
+$application = new Zend_Application(
+    APPLICATION_ENV,
+    APPLICATION_PATH . '/configs/application.ini'
+);
+$application->bootstrap()
+            ->run();
